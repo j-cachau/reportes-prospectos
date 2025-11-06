6e6b51b57a44647b303c779b8a06526bf8657fcc
Julian Cachau <j.cachau@vinson.com.ar>
2025-11-06 00:51:34 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { RANGE, setFiltered } from './state.js';
 import { ensureConfig, loadData, updateLastModifiedKPI } from './data.js';
 import { getFiltered } from './filters.js';
 import { renderKPIs } from './kpis.js';
-import { renderCharts, renderDuracionChart,renderLlamadosTrend,renderProspectosPorDia,renderLlamadosPorHoraUnique, syncAvgTableHeight, renderOmitidasPorHora } from './charts.js';
+import { renderCharts, renderDuracionChart,renderLlamadosTrend,renderProspectosPorDia,renderLlamadosPorHoraUnique, syncAvgTableHeight, renderOmitidasPorHora, renderProsByOperatorQuality } from './charts.js';
 import { renderTables,renderTablaPromediosHora } from './tables.js';
 import { $ } from './utils.js';
 import { initRouter } from './router.js';
@@ -25,6 +25,7 @@ function rerenderAll(){
   renderProspectosPorDia();
   renderTablaPromediosHora();
   renderOmitidasPorHora();
+  renderProsByOperatorQuality();
 }
 
 window.addEventListener('resize', () => {
